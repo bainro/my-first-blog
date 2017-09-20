bfd047c1046cf7a37c542d1564cdaee8c6a85c99
bainro <bainro@onid.oregonstate.edu>
2017-09-19 20:50:16 -0700

    Installed a wysiwyg editor

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Post, Comment
 
-admin.site.register(Post)
+class TinyMCEAdmin(admin.ModelAdmin):
+	class Media: 
+		js = ('/static/js/tiny_mce/tiny_mce.js', '/static/js/tiny_mce/textareas.js',)
+
+admin.site.register(Post, TinyMCEAdmin)
 admin.site.register(Comment)
--- mysite/urls.py
+++ mysite/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     url(r'^accounts/login/$', views.login, name='login'),
     url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
     url(r'', include('blog.urls')),
+    url(r'^tinymce/', include('tinymce.urls')),
 ]
 
 if settings.DEBUG is True:
